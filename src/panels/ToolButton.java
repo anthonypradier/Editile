0bcbd8d40f119da0eecc04fbcbc3b8c704aed789
Anthony <anthony.pradier04@gmail.com>
2025-06-02 07:48:28 +0200

    added buttons to the tool panel, not working yet

---
+++ src/panels/ToolButton.java
@@ -0,0 +1,9 @@
+package panels;
+
+import javax.swing.*;
+
+public class ToolButton extends JButton {
+    public ToolButton(final String name) {
+        super(name);
+    }
+}
--- src/panels/ToolsPanel.java
+++ src/panels/ToolsPanel.java
@@ -4,13 +4,20 @@ import main.App;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class ToolsPanel extends JPanel {
     private App app;
+    private JPanel btnsPanel;
+    private HashMap<String, ToolButton> buttonsMap;
+    private static final String[] BUTTON_NAME = {"pen", "rubber"};
 
     public ToolsPanel(final App app) {
         super();
         this.app = app;
+        this.buttonsMap = new HashMap<String, ToolButton>();
+        this.btnsPanel = new JPanel();
+        this.initButtons();
         this.build();
     }
 
@@ -19,5 +26,24 @@ public class ToolsPanel extends JPanel {
         this.setMinimumSize(dim);
         this.setPreferredSize(dim);
         this.setBackground(Color.orange);
+        this.setLayout(new GridLayout());
+
+        for(String btnName : BUTTON_NAME) {
+            this.add(this.buttonsMap.get(btnName));
+        }
+
+    }
+
+    private void initButtons() {
+        for(String btnName : BUTTON_NAME) {
+            this.buttonsMap.put(btnName, new ToolButton(btnName));
+        }
+        for(String func : this.buttonsMap.keySet()) {
+//            this.buttonsMap.get(func).setPreferredSize(new Dimension(20, 20));
+        }
+    }
+
+    public HashMap<String, ToolButton> getButtons() {
+        return this.buttonsMap;
     }
 }
